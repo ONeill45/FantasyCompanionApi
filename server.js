9bf381f59a54a97ae8c6205deb4e2a90b340c444
Reece Johnson <reece.johnson@macys.com>
2018-08-01 10:13:52 -0400

    added request library. still trying to get a post call to work.

--- server.js
+++ server.js
@@ -1,62 +1,84 @@
-const config = require('./config/db');
-const express = require('express');
+const config = require("./config/db");
+const express = require("express");
 //const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const sql = require('mssql/msnodesqlv8');
+const bodyParser = require("body-parser");
+const sql = require("mssql/msnodesqlv8");
 
 const app = express();
 app.use(bodyParser.json());
 
 var server = app.listen(process.env.PORT || 8000, () => {
-    let port = server.address().port;
-    console.log('We are live on ' + port);
+  let port = server.address().port;
+  console.log("We are live on " + port);
 });
 var executeQuery = function(res, query) {
-    sql.close();
-    sql.connect(config.dbConfig, (err, database) => {
-        if(err) {
+  sql.close();
+  sql.connect(
+    config.dbConfig,
+    (err, database) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        var request = new sql.Request();
+        request.query(query, function(err, response) {
+          if (err) {
             console.log(err);
             res.send(err);
-        }
-        else {
-            var request = new sql.Request();
-            request.query(query, function(err, response){
-                if(err) {
-                    console.log(err);
-                    res.send(err);
-                }
-                else {
-                    console.log(response);
-                    res.send(response);
-                }
-            });
-        }
-        //require('./app/routes')(app, database);
-        
-    });
-}
+          } else {
+            console.log(response);
+            res.send(response);
+          }
+        });
+      }
+      //require('./app/routes')(app, database);
+    }
+  );
+};
 
-app.get('/api/Games', function(req, res){
-    var query = "select * from [dbo].[game]";
-    executeQuery(res, query);
+app.get("/api/Games", function(req, res) {
+  var query = "select * from [dbo].[game]";
+  executeQuery(res, query);
 });
-app.get('/api/Owners', function(req, res){
-    var query = "select * from [dbo].[TeamOwner]";
-    executeQuery(res, query);
+app.get("/api/Owners", function(req, res) {
+  var query = "select * from [dbo].[TeamOwner]";
+  executeQuery(res, query);
 });
-app.get('/api/Seasons', function(req, res){
-    var query = "select * from [dbo].[Season]";
-    executeQuery(res, query);
+app.get("/api/Seasons", function(req, res) {
+  var query = "select * from [dbo].[Season]";
+  executeQuery(res, query);
 });
-app.get('/api/OwnerSeasons', function(req, res){
-    var query = "select * from [dbo].[OwnerSeason]";
-    executeQuery(res, query);
+app.get("/api/OwnerSeasons", function(req, res) {
+  var query = "select * from [dbo].[OwnerSeason]";
+  executeQuery(res, query);
 });
-app.get('/api/GameTypes', function(req, res){
-    var query = "select * from [dbo].[GameType]";
-    executeQuery(res, query);
+app.get("/api/GameTypes", function(req, res) {
+  var query = "select * from [dbo].[GameType]";
+  executeQuery(res, query);
 });
-app.get('/api/GamePlayers', function(req, res){
-    var query = "select * from [dbo].[GamePlayer]";
-    executeQuery(res, query);
+app.get("/api/GamePlayers", function(req, res) {
+  var query = "select * from [dbo].[GamePlayer]";
+  executeQuery(res, query);
+});
+
+var request = require("request");
+const apiKey = "xi1bBWh2TrszX38OVmU7L0dKgjMZtaIJ";
+
+app.get("/api/players/", function(req, res) {
+  console.log("yep im here");
+  request.post("https://profootballapi.com/players", {
+    form: {
+      api_key: "xi1bBWh2TrszX38OVmU7L0dKgjMZtaIJ",
+      stats_type: "offense",
+      player_name: "a.dalton"
+    },
+    function(error, response, body) {
+      if (error) {
+        console.log(error);
+      } else if (response.statusCode == 200) {
+        console.log("200!");
+        res(body);
+      }
+    }
+  });
 });
