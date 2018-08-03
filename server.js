062ba5a527dc5cca3295a2032f696ccd59bdd02d
Reece Johnson <reecerjohnson13@gmail.com>
2018-08-02 23:48:40 -0400

    started working on express router to split code out into controllers

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const bodyParser = require("body-parser");
 const sql = require("mssql/msnodesqlv8");
 
 const app = express();
+const router = express.Router();
 app.use(bodyParser.json());
 
 var server = app.listen(process.env.PORT || 8000, () => {
   let port = server.address().port;
   console.log("We are live on " + port);
 });
+
 var executeQuery = function(res, query) {
   sql.close();
   sql.connect(
@@ -61,25 +63,24 @@ app.get("/api/GamePlayers", function(req, res) {
   executeQuery(res, query);
 });
 
-var request = require("request");
-const apiKey = "xi1bBWh2TrszX38OVmU7L0dKgjMZtaIJ";
+// var request = require("request");
+// const apiKey = "xi1bBWh2TrszX38OVmU7L0dKgjMZtaIJ";
 
-app.get("/api/players/:firstName-:lastName", function(req, res) {
-  const requestData = {
-    api_key: "xi1bBWh2TrszX38OVmU7L0dKgjMZtaIJ",
-    stats_type: "offense",
-    player_name:
-      req.params.firstName.substring(0, 1) + "." + req.params.lastName
-  };
-  request.post(
-    { url: "https://profootballapi.com/players", formData: requestData },
-    function(err, httpResponse, body) {
-      if (err) {
-        console.log(err);
-      } else if (httpResponse.statusCode == 200) {
-        res.send(body);
-      }
-    }
-  );
-});
-//example of above: http://localhost:8000/api/players/tom-brady
+// app.get("/api/players/:firstName-:lastName", function(req, res) {
+//   const requestData = {
+//     api_key: "xi1bBWh2TrszX38OVmU7L0dKgjMZtaIJ",
+//     stats_type: "offense",
+//     player_name:
+//       req.params.firstName.substring(0, 1) + "." + req.params.lastName
+//   };
+//   request.post(
+//     { url: "https://profootballapi.com/players", formData: requestData },
+//     function(err, httpResponse, body) {
+//       if (err) {
+//         console.log(err);
+//       } else if (httpResponse.statusCode == 200) {
+//         res.send(body);
+//       }
+//     }
+//   );
+// });
