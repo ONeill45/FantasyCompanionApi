e1ccdac25a044230e98a2267508bbe00d24281cb
Reece Johnson <reece.johnson@macys.com>
2018-08-23 15:07:47 -0400

    now using Cheerio to parse the HTML body for values.

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const cheerio = require("cheerio");
 
 //example: http://localhost:3000/player/add/andydalton/2495143
 exports.add_player_to_database = function(req, res) {
@@ -6,7 +7,20 @@ exports.add_player_to_database = function(req, res) {
     `http://www.nfl.com/player/${req.params.name}/${req.params.nflId}/profile`,
     (error, response, body) => {
       if (response && response.statusCode) {
-        console.log(body.length);
+        // const newPlayer = new Player();
+
+        const $ = cheerio.load(body);
+        const height = $(".player-info").text();
+        console.log(height);
+
+        // get basic player data
+        // const heightIndex = body.indexOf("Height</strong>:") + 17;
+        // newPlayer.height = body.substring(heightIndex, heightIndex + 3);
+        // console.log(`height ${height}`);
+        // get recent game data
+
+        // get career stats data
+
         res.send();
       } else {
         console.log("error:", error);
